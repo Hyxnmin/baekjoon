bdc5df4fff54c8dafa2a3a70c5cfd3c468e83c01
Hyxnmin <phmi4199@gmail.com>
2022-03-12 23:46:03 +0900

    2022-03-12

--- 2022-03-12/2493 탑.cpp
+++ 2022-03-12/2493 탑.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 stack<pair<int,int>>s;
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
 	int a,n;
 	cin >> a;
 	for (int i = 0; i < a; i++) {
