d13aaf1cb68dd4fb373fdf9d2a4b86f6f9c234a3
Hyxnmin <phmi4199@gmail.com>
2022-03-16 01:56:39 +0900

    2022-03-15

--- 2022-03-15/10101 삼각형 외우기.cpp
+++ 2022-03-15/10101 삼각형 외우기.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define TRIANGLE_ANGLE_MAX 180
+#define TRIANGLE_ANGLE_AVG 60
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	int angle1, angle2, angle3;
 	cin >> angle1 >> angle2 >> angle3;
-	if (angle1 == 60 && angle2 == 60 && angle3 == 60) {
+	if (angle1 == TRIANGLE_ANGLE_AVG && angle2 == TRIANGLE_ANGLE_AVG && angle3 == TRIANGLE_ANGLE_AVG) {
 		cout << "Equilateral";
 	}
-	else if (angle1 + angle2 + angle3 == 180) {
+	else if (angle1 + angle2 + angle3 == TRIANGLE_ANGLE_MAX) {
 		if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3) {
 			cout << "Isosceles";
 		}
@@ -16,7 +18,7 @@ int main() {
 			cout << "Scalene";
 		}
 	}
-	else if (angle1 + angle2 + angle3 != 180) {
+	else if (angle1 + angle2 + angle3 != TRIANGLE_ANGLE_MAX) {
 		cout << "Error";
 	}
 }
