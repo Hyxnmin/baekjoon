ac3d288453074f6c193ead0af175f6bf66b752f9
Hyxnmin <phmi4199@gmail.com>
2022-03-20 00:27:02 +0900

    2022-03-19

---
+++ 2022-03-19/13023 ABCDE.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>graph[2001];
+bool is_visited[2001] = { 0 };
+int ans = 0;
+void find_relationship(int x, int cnt) {
+	if (cnt == 4) {
+		ans = 1;
+		return;
+	}
+	is_visited[x] = 1;
+	for (int i = 0; i < graph[x].size(); i++) {
+		if (is_visited[graph[x][i]] == 0) {
+			find_relationship(graph[x][i], cnt + 1);
+		}
+	}
+	is_visited[x] = 0;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int to, from;
+		cin >> to >> from;
+		graph[to].push_back(from);
+		graph[from].push_back(to);
+	}
+	for (int i = 0; i < n; i++) {
+		find_relationship(i, 0);
+	}
+	cout << ans;
+}
