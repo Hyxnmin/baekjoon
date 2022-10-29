e2fc1b37bb6eaff7860691a245aea942b972ce2a
Hyunmin <phmi4199@gmail.com>
2022-10-30 01:39:37 +0900

    2022-10-29

--- 2022-10-29/10807 개수 세기.cpp
+++ 2022-10-29/10807 개수 세기.cpp
@@ -7,16 +7,27 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int n, v, input, arr[101] = { 0 };
+	int n, v, input, arr1[101] = { 0 }, arr2[101] = { 0 };
 
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> input;
-		arr[input]++;
+		if (input < 0) {
+			arr2[abs(input)]++;
+		}
+		else {
+			arr1[input]++;
+		}
 	}
 
 	cin >> v;
 
-	cout << arr[v];
+	if (v < 0) {
+		cout << arr2[abs(v)];
+	}
+	else {
+		cout << arr1[v];
+	}
+	
 }
