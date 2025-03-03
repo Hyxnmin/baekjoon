9df5f58ec0840b53a93a59a6262afe66541229b2
Hyxnmin <hyxnminp@gmail.com>
2025-03-03 20:37:07 +0900

    2025-03-03

---
+++ 2025-03/2025-03-03/1964 오각형, 오각형, 오각형....cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[10000001] = { 0 }, n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    arr[0] = 1;
+
+    cin >> n;
+
+    for (long long i = 1; i <= n; ++i) {
+        arr[i] += (arr[i - 1] + 3 * i + 1) % 45678;
+    }
+    cout << arr[n];
+}
