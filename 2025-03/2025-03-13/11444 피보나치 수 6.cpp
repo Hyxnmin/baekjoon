cbe711b1c6e7ad2f35f317be68cae9ea40391c3e
Hyxnmin <hyxnminp@gmail.com>
2025-03-13 17:24:33 +0900

    2025-03-13

---
+++ 2025-03/2025-03-13/11444 피보나치 수 6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<long long, long long> m;
+const int MOD = 1000000007;
+
+long long fibonacci(long long n) {
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    if (n == 3) return 2;
+
+    if (m.find(n) != m.end()) return m[n];
+
+    long long p, q;
+
+    if (n % 2 == 1) {
+        p = fibonacci(n / 2 + 1) % MOD;
+        q = fibonacci(n / 2) % MOD;
+        m[n / 2 + 1] = p;
+        m[n / 2] = q;
+        return m[n] = (p * p % MOD + q * q % MOD) % MOD;
+    }
+    else {
+        p = fibonacci(n / 2 + 1) % MOD;
+        q = fibonacci(n / 2 - 1) % MOD;
+        m[n / 2 + 1] = p;
+        m[n / 2 - 1] = q;
+        return m[n] = ((p * p % MOD - q * q % MOD) + MOD) % MOD;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    long long n;
+    cin >> n;
+    cout << fibonacci(n) << "\n";
+
+    return 0;
+}
