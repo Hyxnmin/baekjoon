ad85657f070c5dc6eec4b7bb8436a6beb2ff5c8d
Hyxnmin <hyxnminp@gmail.com>
2025-03-20 00:35:24 +0900

    2025-03-20

--- 2025-03/2025-03-20/11000 강의실 배정*.cpp
+++ 2025-03/2025-03-20/11000 강의실 배정*.cpp
@@ -1,59 +1,46 @@
 #include <iostream>
 #include <queue>
-
+#include <algorithm>
 
 using namespace std;
 
-struct Time {
+typedef struct Time {
     long long st, ed;
-    Time(long x, long y) : st(x), ed(y) {}
-};
+}Time;
 
-struct comp {
-    bool operator()(Time x, Time y) {
-        if (x.ed == y.ed)return x.st < y.st;
-        return x.ed > y.ed;
-    }
-};
+Time arr[200001];
 
-priority_queue<Time, vector<Time>, comp>pq;
-queue<Time>q;
+bool comp(Time x, Time y) {
+    if (x.st == y.st)return x.ed < y.ed;
+    return x.st < y.st;
+}
+
+priority_queue<int, vector<int>, greater<int>>pq;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    long n, s, t, tmp, cnt = 0;
+    int n;
 
     cin >> n;
 
 
-    for (int i = 0;i < n;++i) {
-        cin >> s >> t;
-        pq.push({ s,t });
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i].st >> arr[i].ed;
     }
 
-    while (!pq.empty()) {
-        tmp = pq.top().ed;
-        pq.pop();
+    sort(arr, arr + n, comp);
 
-        while (!pq.empty()) {
-            if (tmp <= pq.top().st) {
-                tmp = pq.top().ed;
-                pq.pop();
-            }
-            else {
-                q.push(pq.top());
-                pq.pop();
-            }
+    pq.push(arr[0].ed);
+
+    for (int i = 1; i < n; ++i) {
+        pq.push(arr[i].ed);
+        if (pq.top() <= arr[i].st) {
+            pq.pop();
         }
-        while (!q.empty()) {
-            pq.push(q.front());
-            q.pop();
-        }
-        cnt++;
     }
 
-    cout << cnt;
+    cout << pq.size();
 
 }
---
+++ 2025-03/2025-03-20/2525 오븐 시계.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    b += c;
+    if (b >= 60) {
+        a += b / 60;
+        b %= 60;
+        if (a >= 24) {
+            a %= 24;
+        }
+    }
+
+    cout << a << ' ' << b;
+}
