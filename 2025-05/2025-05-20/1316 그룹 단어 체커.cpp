cf5c3b85ec43caa33566ed56afca8fa7569a6a58
Hyxnmin <hyxnminp@gmail.com>
2025-05-20 01:03:21 +0900

    2025-05-20

---
+++ 2025-05/2025-05-20/1316 그룹 단어 체커.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+
+map<char, int>m;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, cnt = 0;
+    string str;
+
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0;i < n;++i) {
+        getline(cin, str);
+        for (int j = 0;j < str.size() - 1;++j) {
+            if (str[j] != str[j + 1]) {
+                if (m.find(str[j+1]) != m.end()) {
+                    goto nxt;
+                }
+                m.insert({ str[j], 1 });
+            }
+        }
+        cnt++;
+    nxt:
+        m.clear();
+    }
+
+    cout << cnt;
+}
