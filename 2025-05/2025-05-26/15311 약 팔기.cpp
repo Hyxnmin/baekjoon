6decaa567203b52431560facc1b7ec3b05ac2126
Hyxnmin <hyxnminp@gmail.com>
2025-05-27 04:39:01 +0900

    2025-05-26

---
+++ 2025-05/2025-05-26/15311 약 팔기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+
+    cin >> n;
+    cout << 2000 << '\n';
+    for (int i = 0; i < 1000; ++i)cout << 1 << '\n';
+    for (int i = 0; i < 1000; ++i)cout << 1000 << '\n';
+
+    return 0;
+}
